5a5d14504665ca60e6fd9355e97e9b3c0678f4a7
shawnchoi8 <bychoise8@gmail.com>
2025-02-06 18:04:28 +0200

    Entity Class Development 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     public String city;
     public String street;
     public String zipcode;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
